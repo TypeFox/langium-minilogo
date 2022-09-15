ae33381f1ce8c2bc768e842d7775282bfa4391e6
Benjamin Friedman Wilson <friedman.benjamin@gmail.com>
2022-09-15 20:04:16 +0200

    reworked validator & generator

--- src/cli/generator.ts
+++ src/cli/generator.ts
@@ -18,47 +18,34 @@ type DrawingState = {
 
 export function generateJavaScript(model: Model, filePath: string, destination: string | undefined): string {
     const data = extractDestinationAndName(filePath, destination);
-    const generatedFilePathJS = `${path.join(data.destination, data.name)}.js`;
+    const generatedFilePathJS = path.join(data.destination, 'mini-logo.js');
     const generatedFilePathHTML = `${path.join(data.destination, data.name)}.html`;
 
     if (!fs.existsSync(data.destination)) {
-        fs.mkdirSync(data.destination);
+        fs.mkdirSync(data.destination, { recursive: true });
     }
 
-    // write out an HTML file for our canvas ops
-    const fileNodeHTML = new CompositeGeneratorNode();
-    // crude literal writing
-    fileNodeHTML.append(
-        "<!DOCTYPE html>\n",
-        "<html>\n",
-        "<head>\n",
-        "<meta charset='utf-8'>",
-        `<script src='${data.name}.js' async></script>`,
-        "<title>MiniLogo to HTML Canvas via Langium</title>",
-        "</head>\n",
-        "<body>\n",
-        "<h1 style='text-align:center'>MiniLogo -&gt; to HTML Canvas via Langium</h1>\n",
-        "<canvas id='minilogo-canvas' width=500 height=600 style='display:block;margin:8px auto;text-align:center;border:2px solid #ccc'></canvas>\n",
-        "</body>\n",
-        "</html>");
-    fs.writeFileSync(generatedFilePathHTML, processGeneratorNode(fileNodeHTML));
+    fs.writeFileSync(generatedFilePathHTML, fs.readFileSync('./src/assets/index.html'));
 
     // write a JS file that draws on the HTML canvas from
-    // we'll start a `run_minilogo` function, and then compile our 
+    // we'll start a `run_minilogo` function, and then generate our 
     // minilogo program into equivalent draw instructions for the canvas
     const fileNodeJS = new CompositeGeneratorNode();
     fileNodeJS.append(
-        '"use strict";',
-        NL, NL,
-        "function run_minilogo() {\n", 
-        "\tlet canvas = document.getElementById('minilogo-canvas');\n",
-        // simple 2d context to draw with
-        "\tlet context = canvas.getContext('2d');\n",
-        // move off from the corner, so the cursor can be seen
-        "\tcontext.moveTo(150,150);\n",
-        // start a path
-        "\tcontext.beginPath();\n",
-        NL);
+        '"use strict";', NL, NL,
+        "function run_minilogo() {", NL
+    );
+
+    fileNodeJS.indent(i => {
+        i.append(
+            "let canvas = document.getElementById('minilogo-canvas');", NL,
+            "let context = canvas.getContext('2d');", NL,
+            "let mode = 'down';", NL,
+            "context.strokeStyle = 'white';", NL,
+            "context.moveTo(150,150);", NL,
+            "context.beginPath();", NL
+        );
+    });
 
     // setup drawing state
     let state : DrawingState = {
@@ -72,29 +59,33 @@ export function generateJavaScript(model: Model, filePath: string, destination: 
 
     // write the mini logo function using the commands we produced, and write them out
     let computedJS = model.stmts.flatMap(s => evalStmt(s,env,state));
-    if(computedJS != undefined) {
-        // good to go
-        computedJS.forEach(line => fileNodeJS.append(line + ";\n"));
-
+    if(computedJS !== undefined) {
+        fileNodeJS.indent(i => {
+            // good to go
+            computedJS.forEach(line => {
+                if (line) {
+                    i.append(line, ';', NL);
+                }
+            });
+        });
+        
     } else {
-        // failed to compile to JS
-        throw new Error("Failed to compile MiniLogo program to JS draw instructions!"); 
+        // failed to generate JS
+        throw new Error("Failed to generate MiniLogo program to JS draw instructions!");
 
     }
 
     // check to cap off the previous path, if still active
     if(state.drawing) {
-        fileNodeJS.append("\tcontext.stroke();");
+        fileNodeJS.append("\tcontext.stroke()");
     }
 
     // cap off our JS
     fileNodeJS.append(
-        "}\n",
-        "window.onload = run_minilogo();\n", NL);
+        "}", NL,
+        "window.onload = run_minilogo()", NL, NL
+    );
 
-    if (!fs.existsSync(data.destination)) {
-        fs.mkdirSync(data.destination, { recursive: true });
-    }
     fs.writeFileSync(generatedFilePathJS, processGeneratorNode(fileNodeJS));
 
     return generatedFilePathHTML;
@@ -105,7 +96,7 @@ export function generateJavaScript(model: Model, filePath: string, destination: 
 function evalStmt(stmt: Stmt, env: MiniLogoGenEnv, state: DrawingState) : (string | undefined)[] {
     if(isPen(stmt)) {
         // pen change
-        if(stmt.mode == 'up') {
+        if(stmt.mode === 'up') {
             // lift pen up & complete existing path
             state.drawing = false;
             return ["context.stroke()"];
@@ -113,7 +104,7 @@ function evalStmt(stmt: Stmt, env: MiniLogoGenEnv, state: DrawingState) : (strin
         } else {
             // push pen down & start new path, moving to the current point as well
             state.drawing = true;
-            return [`context.beginPath()`,`context.moveTo(${state.px},${state.py})`];
+            return [`context.beginPath()`, `context.moveTo(${state.px},${state.py})`];
 
         }
 
@@ -125,128 +116,86 @@ function evalStmt(stmt: Stmt, env: MiniLogoGenEnv, state: DrawingState) : (strin
         if(state.drawing) {
             // draw a line
             return [`context.lineTo(${state.px},${state.py})`];
-
         } else {
             // or update the pen's position
             return [`context.moveTo(${state.px},${state.py})`];
-
         }
 
     } else if(isMacro(stmt)) {
         // get the cross ref & validate it
         let macro = stmt.def.ref;
-        if(macro == undefined) {
+        if(macro === undefined) {
             throw new Error(`Attempted to reference an undefined macro: ${stmt.def}`);
         }
 
         // original env to restore post evaluation
-        let origEnv = new Map<string,number>(env);
+        let macroEnv = new Map(env);
 
         // produce pairs of string & exprs, using a tmp env
         // this is important to avoid mixing of params that are only present in the tmp env w/ our actual env
         let tmpEnv = new Map<string, number>();
 
-        macro.params.map((elm, idx) => tmpEnv.set(elm.name, evalExprWithEnv(stmt.args[idx], env)));
+        macro.params.map((elm, idx) => tmpEnv.set(elm.name, evalExprWithEnv(stmt.args[idx], macroEnv)));
+        tmpEnv.forEach((v,k) => macroEnv.set(k,v));
 
-        // update miniEnv w/ temp env
-        tmpEnv.forEach((v,k) => env.set(k,v));
-
-        // evalute the body
-        let computedJS = macro.body.flatMap(s => evalStmt(s, env, state));
-
-        // reset env
-        env = origEnv;
-
-        return computedJS;
+        return macro.body.flatMap(s => evalStmt(s, macroEnv, state));
 
     } else if(isFor(stmt)) {
         // for loop bounds
         let vi = evalExprWithEnv(stmt.e1, env);
         let ve = evalExprWithEnv(stmt.e2, env);
 
-        // clone binding to avoid clobbering any pre-existing binding
-        let origEnv = new Map(env);
-
-        let fvar = stmt.var.name;
-
-        // loop until we've performed all actions
         let computedJS : (string | undefined)[] = [];
+        
+        // perform loop
+        const loopEnv = new Map(env);
         while(vi < ve) {
-            // update env
-            env.set(fvar, vi);
-            // manage effects from individual evaluations
-            let rEnv = new Map(env);
-            // evalute body, and capture results
+            loopEnv.set(stmt.var.name, vi++);
             stmt.body.forEach(s => {
-                // get results
-                computedJS = computedJS.concat(evalStmt(s,env,state));
-                // clear effects
-                env = rEnv;
+                computedJS = computedJS.concat(evalStmt(s, new Map(loopEnv), state));
             });
-            // step forward
-            vi++;
         }
-
-        // restore binding to prior state
-        env = origEnv;
 
         return computedJS;
 
     } else {
-        // unrecognized result...
         return [];
-
     }
 }
 
 // Evaluates exprs to final vals for emission
-function evalExprWithEnv(e: Expr, env: MiniLogoGenEnv): number | never {
+function evalExprWithEnv(e: Expr, env: MiniLogoGenEnv): number {
     if(isLit(e)) {
-        // literal value to use
         return e.val;
 
     } else if(isRef(e)) {
-        // reference to try and find
-        let r = e.val.ref;
-        if(r != undefined) {
-            let v = env.get(r.name);
-            if(v !== undefined) {
-                return v;
-            }
+        const v = env.get(e.val.ref?.name ?? '');
+        if (v !== undefined) {
+            return v;
         }
-        // shouldn't get to this point w/ cross refs working...
-        throw new Error(`Attempted to lookup an unbound reference '${e.val}' in the env!`);
+        throw new Error(`Attempted to lookup an unbound reference '${e.val}' in the env.`);
 
     } else if(isBinExpr(e)) {
-        // binary expression to resolve
         let opval = e.op.val;
+        let v1    = evalExprWithEnv(e.e1, env);
+        let v2    = evalExprWithEnv(e.e2, env);
 
-        // evaluate these exprs under the given env
-        let v1 = evalExprWithEnv(e.e1, env);
-        let v2 = evalExprWithEnv(e.e2, env);
-
-        // based on our op val, perform the operate binop
         switch(opval) {
             case 'add': return v1 + v2;
             case 'sub': return v1 - v2;
             case 'mul': return v1 * v2;
             case 'div': return v1 / v2;
-            default:  throw new Error(`Unrecognized bin op passed: ${opval}`);
+            default:    throw new Error(`Unrecognized bin op passed: ${opval}`);
         }
         
     } else if (isNegExpr(e)) {
-        // return negated form
-        return evalExprWithEnv(e.ne, env)*-1;
+        return -1 * evalExprWithEnv(e.ne, env);
 
     } else if(isGroup(e)) {
-        // recursively call to evaluate the wrapped expression
         return evalExprWithEnv(e.ge, env);
 
-    } else {
-        // unrecognized expression, but we shouldn't expect to get to this point
-        // TODO would need better clarification on 'what' the expr was while printing out
-        throw new Error(`Unrecognized expression passed for evaluation.`);
-
     }
+
+    throw new Error('Unhandled Expression');
 
 }
--- src/language-server/minilogo-validator.ts
+++ src/language-server/minilogo-validator.ts
@@ -1,5 +1,5 @@
 import { ValidationAcceptor, ValidationCheck, ValidationRegistry } from 'langium';
-import { MiniLogoAstType, Model, isMacro, Def, Stmt, isCmd, Cmd, Expr, isMove, isBinExpr, isGroup, isFor, isNegExpr } from './generated/ast';
+import { Def, MiniLogoAstType, Model } from './generated/ast';
 import type { MiniLogoServices } from './minilogo-module';
 
 /**
@@ -8,136 +8,41 @@ import type { MiniLogoServices } from './minilogo-module';
 type MiniLogoChecks = { [type in MiniLogoAstType]?: ValidationCheck | ValidationCheck[] }
 
 /**
- * MiniLogo env used for validation
- */
-type MiniLogoChkEnv = {
-    defNames : Set<string>,
-    paramNames : Set<string>
-}
-
-/**
  * Registry for validation checks.
  */
 export class MiniLogoValidationRegistry extends ValidationRegistry {
     constructor(services: MiniLogoServices) {
         super(services);
         const validator = services.validation.MiniLogoValidator;
+
         const checks: MiniLogoChecks = {
-            Model: validator.checkModel
+            Model: validator.checkUniqueDefs,
+            Def:   validator.checkUniqueParams
         };
+
         this.register(checks, validator);
     }
 }
 
-/**
- * Implementation of custom validations.
- */
 export class MiniLogoValidator {
 
-    checkModel(model: Model, accept: ValidationAcceptor): void {
-        // load all def names into a set for later validation
-        let env : MiniLogoChkEnv = {
-            defNames: new Set<string>(model.defs.map(d => d.name)),
-            paramNames: new Set<string>()
-        };
-        // check all raw statements, looking for macro calls, make sure those defs exist, otherwise FAIL
-        model.stmts.forEach(s => this.checkStmt(s, env, accept));
-
-        // for each def, chk statements for macro calls & refs, macro calls should exist, refs should be bound, otherwise FAIL
+    checkUniqueDefs(model: Model, accept: ValidationAcceptor): void {
+        const reported = new Set();
         model.defs.forEach(d => {
-            this.checkDef(d, env, accept);
-            // flush any params we may have set
-            env.paramNames.clear();
+            if (reported.has(d.name)) {
+                accept('error',  `Def has non-unique name '${d.name}'.`,  {node: d, property: 'name'});
+            }
+            reported.add(d.name);
         });
     }
 
-    checkStmt(stmt: Stmt, env: MiniLogoChkEnv, accept: ValidationAcceptor): void {
-        if(isMacro(stmt)) {
-            if(stmt.def == undefined) {
-                // bad ref
-                accept(
-                    'error', 
-                    `Undefined macro ${stmt.def}! Try double checking your spelling, or add the correct macro def to your program.`, 
-                    {node: stmt, property: 'def'});
-
-            } else if(stmt.args == undefined) {
-                accept(
-                    'error', 
-                    `Macro call missing arguments, or at least a () pair.`, 
-                    {node: stmt, property: 'def'});
-
-            } else {
-                // verify the expressions passed to this macro
-                stmt.args.forEach(e => this.checkExpr(e, env, accept));
-
+    checkUniqueParams(def: Def, accept: ValidationAcceptor): void {
+        const reported = new Set();
+        def.params.forEach(p => {
+            if (reported.has(p.name)) {
+                accept('error', `Param ${p.name} is non-unique for Def '${def.name}'`, {node: p, property: 'name'});
             }
-
-        } else if(isCmd(stmt)) {
-            // verify this command
-            this.checkCmd(stmt, env, accept);
-
-        } else {
-            // just in case...
-            throw new Error("Undefined statement during validation!");
-
-        }
-    }
-
-    checkDef(def: Def, env: MiniLogoChkEnv, accept: ValidationAcceptor): void {
-        // copy origin param names to restore post-verification
-        let origParamNames = new Set<string>(env.paramNames);
-
-        if(def.params == undefined) {
-            // missing param
-            accept('error',"Missing a parameter listing.",{node: def, property: 'params'});
-            return;
-
-        }
-
-        // collect param names for ref
-        def.params.forEach(p => env.paramNames.add(p.name));
-
-        if(def.body == undefined) {
-            // missing body
-            accept('error',"Missing a body of zero or more statements for this def.",{node: def, property: 'body'});
-            return;
-
-        }
-
-        // verify each statement w/ these params bound
-        def.body.forEach(s => this.checkStmt(s, env, accept));
-
-        // restore original params, if any
-        env.paramNames = origParamNames;
-    }
-
-    checkCmd(cmd: Cmd, env: MiniLogoChkEnv, accept: ValidationAcceptor): void {
-        if (isMove(cmd)) {
-            // verify Move's sub expressions
-            [cmd.ex, cmd.ey].forEach(e => this.checkExpr(e, env, accept));
-
-        } else if(isFor(cmd)) {
-            // verify For loop's body
-            // update env, and check that sub exprs are OK
-            let origPar = env.paramNames.has(cmd.var.name);
-            env.paramNames.add(cmd.var.name);
-            cmd.body.forEach(s => this.checkStmt(s, env, accept));
-            // restore
-            origPar ? "" : env.paramNames.delete(cmd.var.name);
-
-        }
-    }
-
-    checkExpr(expr: Expr, env: MiniLogoChkEnv, accept: ValidationAcceptor): void {
-        if(isBinExpr(expr)) {
-            [expr.e1, expr.e2].forEach(e => this.checkExpr(e, env, accept));
-
-        } else if(isGroup(expr)) {
-            this.checkExpr(expr.ge, env, accept);
-
-        } else if(isNegExpr(expr)) {
-            this.checkExpr(expr.ne, env, accept);
-
-        }
+            reported.add(p.name);
+        });
     }
 }
