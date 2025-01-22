397737b7e1c39530383da58cb4efec48dd5bfd81
Marco Nikander <m4n1k.developer@gmail.com>
2025-01-22 08:56:48 +0100

    Rename index.ts to main.ts (#16)
    * rename index.ts to main.ts and update imports
    
    * updated generated files for current langium version

--- bin/minilogo.js
+++ bin/minilogo.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
-import { default as main } from "../out/cli/index.js";
+import { default as main } from "../out/cli/main.js";
 main();
--- src/language-server/minilogo-module.ts
+++ src/language-server/minilogo-module.ts
@@ -1,6 +1,6 @@
 import { Module, inject } from 'langium';
 import { LangiumServices, PartialLangiumServices, AbstractExecuteCommandHandler, ExecuteCommandAcceptor, DefaultSharedModuleContext, LangiumSharedServices, createDefaultSharedModule, createDefaultModule } from 'langium/lsp';
-import { parseAndGenerate } from '../web/index.js';
+import { parseAndGenerate } from '../web/main.js';
 import { MiniLogoGeneratedModule, MiniLogoGeneratedSharedModule } from './generated/module.js';
 import { MiniLogoValidationRegistry, MiniLogoValidator } from './minilogo-validator.js';
 
